87105964478c6f51ce8d6e35d0adb97bb0b116d5
true1337 <true4game@yandex.ru>
2018-08-30 14:50:45 +0300

    Task custom tag and filter

--- template/templatetags/extras.py
+++ template/templatetags/extras.py
@@ -1,1 +1,24 @@
 from django import template
+
+register = template.Library()
+
+@register.filter(name='inc')
+def inc(value, by):
+    try:
+        res = int(value) + int(by)
+    except (ValueError, TypeError):
+        return ''
+    else:
+        return str(res)
+
+@register.simple_tag(name='division')
+def division(dividend, divisor, to_int=False):
+    try:
+        if to_int:
+            res = int(float(dividend) // float(divisor))
+        else:
+            res = float(dividend) / float(divisor)
+    except (ValueError, TypeError):
+        return ''
+    else:
+        return str(res)
